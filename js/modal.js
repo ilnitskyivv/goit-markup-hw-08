9f7a2e8a1c8b86cff38999b7979ecf1d459778e6
Volodymyr Ilnitskyi <ilnitskyiv2@gmail.com>
2020-06-29 00:48:14 +0300

    Исправляет позиционирование модального окна и развёртывание фонового рисунка в герое

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// скрипт для открытия модального окна
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
@@ -12,3 +13,25 @@
       refs.modal.classList.toggle('is-hidden');
     }
 })();
+
+// скрипт для скролла модалки
+
+  document.querySelector('.hero__button').onclick = () => {
+    document.querySelector('.backdrop').classList.remove('is-hidden');
+    document.querySelector('body').style.overflow = 'hidden';
+  };
+  document.querySelector('.close-button').onclick = () => {
+    document.querySelector('.backdrop').classList.add('is-hidden');
+    document.querySelector('body').style.overflow = 'auto';
+  };
+
+//скрипт для блокировки книпки отправить
+
+  const policy = document.getElementById('checkbox');
+  policy.onchange = () => {
+    if (policy.checked) {
+      document.getElementById('send').removeAttribute('disabled');
+    } else {
+      document.getElementById('send').setAttribute('disabled', 'true');
+    }
+  };
